60c2355dc7b5d9330a1f42329e9b68b630c81c33
Catherine Jeffries <jeffriesc@cwu.edu>
2018-04-20 08:40:34 -0700

    Update to waveform_plots.py, now plots correctly export PYTHONPATH=/Users/bre/clawpacks/5.4.1/anaconda3

--- waveform_plots.py
+++ waveform_plots.py
@@ -2,52 +2,66 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-def create_subplot(number,v, x):
-    fig = plt.Figure()
+def create_subplot(ar_len, col, row):
 
-    ax = plt.subplot(number, v, x)
+    fig = plt.Figure(figsize=(20,20))
+    ylim = [-4, 4]
+
+    ax = plt.subplot(ar_len, col, row)
+    ax.set_ylim(ylim)
+
 
     return ax
-def subplot_count(subplot_number, i):
-    if subplot_number % 2 ==0:
-        v = 2
-        mod = subplot_number / 2
-        x = (i % mod) + 1
 
+
+
+def no_cols(ar_len):
+
+    cols = 1 if ar_len % 2 else 2
+    if ar_len % 2 == 0 :
+        rows = ar_len /2
     else:
-        v = 1
-        x = i + 1
-        mod = subplot_number
-    return(mod, v, x)
+        rows = ar_len
 
-def create_group_plot(subplot_number, i, h1, h2, t1, t2, slip, gauge_depth):
-    mod, v, x = subplot_count(subplot_number)
 
-    ax = create_subplot(mod, v, x)
+    return(cols, rows)
+
+
+def create_group_plot(ar_len, i, h1, h2, t1, t2, slip, gauge_depth, cols, rows):
+    plot_number = i + 1
+
+    ax = create_subplot(rows, cols , plot_number)
 
     # stuff to make the plots look pretty
-    additional_incrememnts = 1
+    additional_increments = 1
     units_per_tick = 15
     seconds_per_increment = 15 * 60
-    num_ticks = int(np.floor(max(t1) / seconds_per_increment) + additional_incrememnts)
+    num_ticks = int(np.floor(max(t1) / seconds_per_increment) + additional_increments)
 
     # Actual plot items
     #plt.figure(1, figsize=(10, 5))  # open the plot figure, declare size
+    plt.subplots_adjust(hspace=0.35, wspace=0.15)
+
     ax.plot(t2, h2, 'b', label='{}m slip'.format(slip))  # plot compare gauge
     ax.plot(t1, h1 * slip, 'r', label='1m slip * {}'.format(slip))  # plot 1m slip gauge * slip
-    plt.xlabel("Time (minutes)", fontsize=16)  # declare the x axis label
-    plt.ylabel("Wave Amplitude (m)", fontsize=16)
+    # plt.xlabel("Time (minutes)", fontsize=16)  # declare the x axis label
+    # plt.ylabel("Wave Amplitude (m)", fontsize=16)
     plt.axhline(y=0.00, xmin=0, xmax=14400, c='black', linewidth=.5, zorder=0)  # draw a zero line
     # declare the x-axis tick marks
-    plt.xticks([seconds_per_increment * i for i in range(num_ticks)],
-               ['%d' % (i * units_per_tick) for i in range(num_ticks)])
+    if plot_number < (ar_len -1):
+        plt.xticks([])
+    else:
+        plt.xticks([seconds_per_increment * i for i in range(num_ticks)],
+                   ['%d' % (i * units_per_tick) for i in range(num_ticks)])
+    if plot_number % 2 == 0:
+        plt.yticks([])
     plt.xlim(0, 5400.)
-    plt.legend(loc='upper right')
+    plt.figlegend(loc='upper right', prop={'size':6})
     plt.title('{}m water depth'.format(gauge_depth))  # title shows what water depth each gauge is located
-    plt.tick_params(labelsize=16)
+    plt.tick_params(labelsize=12)
 
-    #plt.savefig('{}m slip {}m depth'.format(slip, depth))
-    plt.show()
+    plt.savefig('waterdepth.ps')
+
 
 def get_data(file1, file2):
     data = np.loadtxt(file1)
@@ -62,9 +76,11 @@ def get_data(file1, file2):
 
 depth = [5, 10, 15, 20, 30, 40]
 slip = 20
-number = len(depth)
+ar_len = len(depth)
+print(ar_len)
+cols, rows = no_cols(ar_len)
 for i, d in enumerate(depth):
-
+    print(i, d)
     gauge_depth = d # water depth of the gauge
     slip_amount = slip # amount of slip to compare
     gauge_file = 'gauge{:05d}.txt'.format(gauge_depth) # name of the gauge file, uses water depth
@@ -78,4 +94,5 @@ for i, d in enumerate(depth):
     # call the function, pass the parameters
     h1, h2, t1, t2 = get_data(file_original, file_compare)
 
-    create_group_plot(number, i, h1, h2, t1, t2, slip_amount, gauge_depth)
+    create_group_plot(ar_len, i, h1, h2, t1, t2, slip_amount, gauge_depth, cols, rows)
+plt.show()
